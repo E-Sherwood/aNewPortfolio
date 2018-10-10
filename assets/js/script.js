75fba42520734cfd4f4e85fd3bff6ab1bca86824
e-sherwood <34667492+E-Sherwood@users.noreply.github.com>
2018-10-10 01:34:24 -0500

    beginning content + styling, hooked up jquery

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$("#IDC").click(function(){
+    $(".openingText").hide();
+    $("#actually").show()
+})
+$("#actually").click(function(){
+    $(".openingText").show()
+    $("#actually").hide()
+})
