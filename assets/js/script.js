b17932ecee45fdaf308250c87a8045f02c7fc21d
e-sherwood <34667492+E-Sherwood@users.noreply.github.com>
2018-10-11 02:58:48 -0500

    portfolio styling + beginning JS work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,29 @@
-$("#IDC").click(function(){
+$("#IDC").click(function () {
     $(".openingText").hide();
     $("#actually").show()
 })
-$("#actually").click(function(){
+$("#actually").click(function () {
     $(".openingText").show()
     $("#actually").hide()
 })
+
+var scModal = $("#scModal");
+var scButton = $("#scImages");
+var scClose = $("#scClose");
+scButton.on("click", function () {
+    $("#scModal").css("display", "block");
+    console.log("open sesame")
+})
+scClose.on("click", function () {
+    $("#scModal").css("display", "none");
+})
+var scImageArray = ["spaceCat-Playtime", "spaceCat-Gameover", "spaceCat-Scripts", "SpaceCat-Scratch"];
+$("#nextButton").on("click", function () {
+    for (var i = 0; i < scImageArray.length; i++) {
+        $("#test").prop("src", "assets/images/" + scImageArray[i] +".png");
+        
+         if (i === scImageArray.length) {
+             i === 0;
+         }
+    }
+})    
