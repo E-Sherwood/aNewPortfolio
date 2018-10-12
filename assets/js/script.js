5addb9927a6efd6af12ae673833608d82d3c3dda
e-sherwood <34667492+E-Sherwood@users.noreply.github.com>
2018-10-12 17:13:52 -0500

    working modals on portfolio page, extra screenshots of projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// hide/show portfolio beginning text
 $("#IDC").click(function () {
     $(".openingText").hide();
     $("#actually").show()
@@ -6,24 +7,91 @@ $("#actually").click(function () {
     $(".openingText").show()
     $("#actually").hide()
 })
-
-var scModal = $("#scModal");
-var scButton = $("#scImages");
-var scClose = $("#scClose");
-scButton.on("click", function () {
+// arrays for project galleries
+var nekroImageArray = []
+var scImageArray = ["spaceCat-Playtime", "spaceCat-Gameover", "spaceCat-Scripts", "SpaceCat-Scratch"];
+var psyImageArray = ["psygame2", "psygame3", "psygame4"];
+var mageImageArray = ["mageFightGame", "mageFight2", "mageFight3", "mageFight4"]
+var cosmicImageArray= ["cosmic2", "cosmicTriviaGame", "cosmic3", "cosmic4"]
+var gifImageArray = ["brutal", "brutal2", "brutal3", "brutal4"]
+var razputinImageArray = []
+$("#scImages").on("click", function (){
     $("#scModal").css("display", "block");
     console.log("open sesame")
+    for (var i = 0; i < scImageArray.length; i++) {
+        var newImage = $('<img>');
+        newImage.prop("style", "height: 550px; width: 700px; padding-bottom: 10px")
+        newImage.prop("src", "assets/images/"+scImageArray[i]+".png")
+        $("#SC").append(newImage);
+    }
 })
-scClose.on("click", function () {
+$("#scClose").on("click", function () {
     $("#scModal").css("display", "none");
 })
-var scImageArray = ["spaceCat-Playtime", "spaceCat-Gameover", "spaceCat-Scripts", "SpaceCat-Scratch"];
-$("#nextButton").on("click", function () {
-    for (var i = 0; i < scImageArray.length; i++) {
-        $("#test").prop("src", "assets/images/" + scImageArray[i] +".png");
-        
-         if (i === scImageArray.length) {
-             i === 0;
-         }
+    
+$("#psyImages").on("click", function (){
+    $("#psyModal").css("display", "block");
+    console.log("open sesame")
+    for (var i = 0; i < psyImageArray.length; i++) {
+        var newImage = $('<img>');
+        newImage.prop("style", "height: 550px; width: 700px; padding-bottom: 10px")
+        newImage.prop("src", "assets/images/"+psyImageArray[i]+".png")
+        $("#PSY").append(newImage);
     }
-})    
+})
+$("#psyClose").on("click", function(){
+    $("#psyModal").css("display", "none");
+})
+$("#mageImages").on("click", function (){
+    $("#mageModal").css("display", "block");
+    console.log("open sesame")
+    for (var i = 0; i < psyImageArray.length; i++) {
+        var newImage = $('<img>');
+        newImage.prop("style", "height: 550px; width: 700px; padding-bottom: 10px")
+        newImage.prop("src", "assets/images/"+mageImageArray[i]+".png")
+        $("#MF").append(newImage);
+    }
+})
+$("#mageClose").on("click", function(){
+    $("#mageModal").css("display", "none");
+})
+
+$("#cosmicImages").on("click", function (){
+    $("#cosmicModal").css("display", "block");
+    console.log("open sesame")
+    for (var i = 0; i < cosmicImageArray.length; i++) {
+        var newImage = $('<img>');
+        newImage.prop("style", "height: 550px; width: 700px; padding-bottom: 10px")
+        newImage.prop("src", "assets/images/"+cosmicImageArray[i]+".png")
+        $("#CT").append(newImage);
+    }
+})
+$("#cosmicClose").on("click", function(){
+    $("#cosmicModal").css("display", "none");
+})
+$("#gifImages").on("click", function (){
+    $("#gifModal").css("display", "block");
+    console.log("open sesame")
+    for (var i = 0; i < gifImageArray.length; i++) {
+        var newImage = $('<img>');
+        newImage.prop("style", "height: 550px; width: 700px; padding-bottom: 10px")
+        newImage.prop("src", "assets/images/"+gifImageArray[i]+".png")
+        $("#BG").append(newImage);
+    }
+})
+$("#gifClose").on("click", function(){
+    $("#gifModal").css("display", "none");
+})
+$("#razImages").on("click", function (){
+    $("#razModal").css("display", "block");
+    console.log("open sesame")
+    for (var i = 0; i < razputinImageArray.length; i++) {
+        var newImage = $('<img>');
+        newImage.prop("style", "height: 550px; width: 700px; padding-bottom: 10px")
+        newImage.prop("src", "assets/images/"+razputinImageArray[i]+".png")
+        $("#RAZ").append(newImage);
+    }
+})
+$("#razClose").on("click", function(){
+    $("#razModal").css("display", "none");
+})
